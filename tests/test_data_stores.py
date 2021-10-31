4bb8ec41df6596b0d2475dce65a526820079f04b
Fran Hrženjak <fran.hrzenjak@gmail.com>
2021-10-31 11:13:13 +0100

    use a datetime index

--- tests/test_data_stores.py
+++ tests/test_data_stores.py
@@ -15,11 +15,11 @@ def mocked_client():
     p_retrieve = p_client.datapoints.retrieve.return_value
     p_retrieve.to_pandas.return_value = pd.DataFrame(
         {"houston.ro.REMOTE_AI[22]": [0.003925000131130218, np.nan, np.nan]},
-        index=[
+        index=pd.DatetimeIndex([
             "2020-01-01T00:00:53",
             "2020-01-01T00:00:54",
             "2020-01-01T00:00:55",
-        ],
+        ]),
     )
     return p_client
 
@@ -30,20 +30,12 @@ def patch_retrieve_datapoints_df(mocker):
         "cognite_synthetic_tags.data_stores.retrieve_datapoints_df",
     )
     p_retrieve_datapoints_df.return_value = pd.DataFrame(
-        columns=["houston.ro.REMOTE_AI[22]"],
-        data=[
-            [0.003925000131130218],
-            [np.nan],
-            [np.nan],
-        ],
-        index=map(
-            lambda val: datetime.strptime(val, "%Y-%m-%dT%H:%M:%S"),
-            [
-                "2020-01-01T00:00:53",
-                "2020-01-01T00:00:54",
-                "2020-01-01T00:00:55",
-            ],
-        ),
+        {"houston.ro.REMOTE_AI[22]": [0.003925000131130218, np.nan, np.nan]},
+        index=pd.DatetimeIndex([
+            "2020-01-01T00:00:53",
+            "2020-01-01T00:00:54",
+            "2020-01-01T00:00:55",
+        ]),
     )
     return p_retrieve_datapoints_df
 
