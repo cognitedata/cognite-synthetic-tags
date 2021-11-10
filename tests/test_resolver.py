69a65c2e87f4e6222b4df533b074370ea081b621
Fran Hrženjak <fran.hrzenjak@gmail.com>
2021-11-10 11:58:26 +0100

    import fix

--- tests/test_resolver.py
+++ tests/test_resolver.py
@@ -3,7 +3,7 @@ from unittest.mock import patch
 
 import pandas as pd
 import pytest
-from pandas._testing import assert_frame_equal
+from pandas.testing import assert_frame_equal
 
 from cognite_synthetic_tags import Tag, TagResolver
 
